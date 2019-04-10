b86f00f180e8c6eac5822ee2e0d8774d8f03a8fb
yerlan.kussainov <erganatkusainov@mail.ru>
2019-04-11 02:10:22 +0700

    Deleted useless methods in checkoutController

--- src/Controller/CheckoutController.php
+++ src/Controller/CheckoutController.php
@@ -21,16 +21,6 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
 class CheckoutController extends AbstractController
 {
     /**
-     * @Route("/", name="checkout_index", methods={"GET"})
-     */
-    public function index(CheckoutRepository $checkoutRepository): Response
-    {
-        return $this->render('checkout/index.html.twig', [
-            'checkouts' => $checkoutRepository->findAll(),
-        ]);
-    }
-
-    /**
      * @Route("/cart", name="checkout_cart", methods={"POST"})
      */
     public function shoppingCart(Request $request)
@@ -86,58 +76,12 @@ class CheckoutController extends AbstractController
             $entityManager->persist($checkout);
             $entityManager->flush();
 
-            return $this->redirectToRoute('checkout_index');
+            return $this->redirectToRoute('myhistory');
         }
 
         return $this->render('checkout/new.html.twig', [
             'checkout' => $checkout,
             'form' => $form->createView(),
         ]);
-    }
-
-    /**
-     * @Route("/{id}", name="checkout_show", methods={"GET"})
-     */
-    public function show(Checkout $checkout): Response
-    {
-        return $this->render('checkout/show.html.twig', [
-            'checkout' => $checkout,
-        ]);
-    }
-
-    /**
-     * @Route("/{id}/edit", name="checkout_edit", methods={"GET","POST"})
-     */
-    public function edit(Request $request, Checkout $checkout): Response
-    {
-        $form = $this->createForm(CheckoutType::class, $checkout);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('checkout_index', [
-                'id' => $checkout->getId(),
-            ]);
-        }
-
-        return $this->render('checkout/edit.html.twig', [
-            'checkout' => $checkout,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="checkout_delete", methods={"DELETE"})
-     */
-    public function delete(Request $request, Checkout $checkout): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$checkout->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($checkout);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('checkout_index');
     }
 }
